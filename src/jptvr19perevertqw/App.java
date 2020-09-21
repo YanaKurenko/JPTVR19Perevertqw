484407e5022f2ee890dd4ba7c8417987f3b8552e
Yana Kurenko <jana.kurenko@ivkhk.ee>
2020-09-21 10:41:54 +0300

    Рабочая программа

--- src/jptvr19perevertqw/App.java
+++ src/jptvr19perevertqw/App.java
@@ -5,6 +5,8 @@
  */
 package jptvr19perevertqw;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -12,5 +14,35 @@ package jptvr19perevertqw;
 class App {
     public void run(){
         System.out.println("--- Перевертыш ---");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите строку символов");
+        String str =  scanner.nextLine();
+        System.out.println("В этой строке символов" +str );
+        char [] chStr = str.toCharArray();
+        char [] reversStr = new char[chStr.length];
+        for (int i = 0; i < reversStr.length; i++) {
+            reversStr[i]= chStr[reversStr.length -1-i];
+        }
+        String rStr = reversStr.toString();
+        System.out.println("Обратная строка" + new String (reversStr));
+        System.out.print("Второе слово в строке: ");
+        int firstSpace = str.indexOf(" ");
+        String str2 = str.substring(firstSpace+1);
+        int nextSpace = str2.indexOf(" ");
+        String secondWord;
+        if (nextSpace > 0){
+            secondWord =  str.substring(firstSpace+1, nextSpace + firstSpace +1);
+        }else{
+            secondWord = str2;
+        }
+        System.out.println(secondWord);
+        
+        
+       
+        
     }
+    
+    
+    
 }
+
